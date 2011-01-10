5be362871cc28f5fe6583e10522ca7f00bfd1eef
beta <beta@e52b1c2b-7d5d-4ba9-ac39-b163d13cfa43>
2011-01-10 00:42:31 +0000

    removing the extraction action on organisms list, because it's still unimplemented

--- branches/v2/apps/rp/modules/organism/templates/_list_actions.php
+++ branches/v2/apps/rp/modules/organism/templates/_list_actions.php
@@ -1,2 +1,2 @@
 <?php echo $helper->linkToNew  (array(  'params' => 'class= fg-button ui-state-default  ',  'class_suffix' => 'new',  'label' => 'New',)) ?>
-<?php echo $helper->linkToExtraAction(array(  'params' => 'class= fg-button ui-state-default  ',  'action' => 'csv',  'extra-icon' => 'list', 'label' => 'Extract',)) ?>
+<?php //echo $helper->linkToExtraAction(array(  'params' => 'class= fg-button ui-state-default  ',  'action' => 'csv',  'extra-icon' => 'list', 'label' => 'Extract',)) ?>
