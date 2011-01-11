7cdca71b57623445fe677dd5c1a52003ac812b06
beta <beta@e52b1c2b-7d5d-4ba9-ac39-b163d13cfa43>
2011-01-11 11:58:52 +0000

    correcting sfAdminThemejRollerPlugin for batch_action and CSRF protection correcting sfAdminThemejRollerPlugin for changing pagination directly throught the page number

--- branches/v2/plugins/sfAdminThemejRollerPlugin/data/generator/sfDoctrineModule/jroller/template/templates/_pagination.php
+++ branches/v2/plugins/sfAdminThemejRollerPlugin/data/generator/sfDoctrineModule/jroller/template/templates/_pagination.php
@@ -26,7 +26,7 @@ $last = $first + $pager->getMaxPerPage() - 1;
 
               <td align="center">
                 [?php echo __('Page') ?]
-                <input type="text" name="page" value="[?php echo $pager->getPage() ?]" maxlength="7" size="2" />
+                <input type="text" onkeypress="javascript: if(event.keyCode == 13){ window.location = '[?php echo url_for('contact/index') ?]?page='+this.value; this.form.onsubmit = function(){ return false;}; }" name="page" value="[?php echo $pager->getPage() ?]" maxlength="7" size="2" />
                 [?php echo __('of %1%', array('%1%' => $pager->getLastPage())) ?]
             	</td>
 
