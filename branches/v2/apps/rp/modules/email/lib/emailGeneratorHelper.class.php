0dce75b0acb9f4e0a695fcd9289519969132ef9b
beta <beta@e52b1c2b-7d5d-4ba9-ac39-b163d13cfa43>
2011-01-13 19:23:53 +0000

    adding the symfony module "email" to prepare emailing functionnalities

---
+++ branches/v2/apps/rp/modules/email/lib/emailGeneratorHelper.class.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * email module helper.
+ *
+ * @package    e-venement
+ * @subpackage email
+ * @author     Baptiste SIMON <baptiste.simon AT e-glop.net>
+ * @version    SVN: $Id: helper.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
+ */
+class emailGeneratorHelper extends BaseEmailGeneratorHelper
+{
+}
