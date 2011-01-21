50078a72ebd515c949d2a880adca94ca53e39224
beta <beta@e52b1c2b-7d5d-4ba9-ac39-b163d13cfa43>
2011-01-21 18:16:57 +0000

    adding a log for authentication process

--- branches/v2/apps/rp/config/rpConfiguration.class.php
+++ branches/v2/apps/rp/config/rpConfiguration.class.php
@@ -7,6 +7,7 @@ class rpConfiguration extends sfApplicationConfiguration
     sfConfig::set('sf_app_template_dir', sfConfig::get('sf_apps_dir') . '/templates');
     
     $this->dispatcher->connect('admin.save_object', array($this, 'setSpecialFlash'));
+    $this->dispatcher->connect('user.change_authentication', array($this, 'logAuthentication'));
   }
   
   public function setSpecialFlash(sfEvent $event)
@@ -17,5 +18,21 @@ class rpConfiguration extends sfApplicationConfiguration
     if ( $params['object'] instanceof Email )
     if ( $params['object']->not_a_test )
       $event->getSubject()->getUser()->setFlash('success',"Your email have been sent correctly.");
+  }
+  public function logAuthentication(sfEvent $event)
+  {
+    $params   = $event->getParameters();
+    $user     = sfContext::getInstance()->getUser();
+    $request  = sfContext::getInstance()->getRequest();
+    
+    $auth = new Authentication();
+    $auth->sf_guard_user_id = $user->getId();
+    $auth->description      = $user;
+    $auth->ip_address       = $request->getHttpHeader('addr','remote');
+    $auth->user_agent       = $request->getHttpHeader('User-Agent');
+    $auth->referer          = $request->getReferer();
+    $auth->success          = $params['authenticated'];
+    
+    $auth->save();
   }
 }
