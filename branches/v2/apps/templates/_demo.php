08e9bf1e95c34e120c8959b82750bc4085dfd049
beta <beta@e52b1c2b-7d5d-4ba9-ac39-b163d13cfa43>
2011-01-24 00:37:11 +0000

    updating the demo partial

--- branches/v2/apps/templates/_demo.php
+++ branches/v2/apps/templates/_demo.php
@@ -4,6 +4,7 @@
   <li><strong>mod</strong> / mod     : capacité à modifier des données</li>
   <li><strong>admin</strong> / admin : privilèges proches maximum</li>
 </ul>
+<p style="margin-bottom: 10px;">En cas de problème, <a href="mailto:admin_AT_libre-informatique.fr">nous contacter</a></p>
 <p>
   <strong>Ce logiciel n'est pas un site web.</strong>
 </p>
