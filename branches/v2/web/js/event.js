7e4d320c3baf3d7028434cc591db6b9cadc10671
beta <beta@e52b1c2b-7d5d-4ba9-ac39-b163d13cfa43>
2011-01-25 12:27:59 +0000

    some improvements for event forms

--- branches/v2/web/js/event.js
+++ branches/v2/web/js/event.js
@@ -2,7 +2,7 @@ function manifestations_loaded(data)
 {
   $('#more .manifestations').html($(data).find(' .sf_admin_list'));
   $('#more .manifestations tfoot a[href]').click(function(){
-    $.get($(this).attr('href'),group_manifestations_loaded);
+    $.get($(this).attr('href'),manifestations_loaded);
     return false;
   });
 }
