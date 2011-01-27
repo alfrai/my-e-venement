1288eccaa4df9a16fb5e71b023d40b22ad6c50cb
beta <beta@e52b1c2b-7d5d-4ba9-ac39-b163d13cfa43>
2011-01-27 19:54:04 +0000

    to get generic code ... put some lines in an external template from config/ProjectConfiguration.class.php

--- branches/v2/config/autoload.inc.php
+++ branches/v2/config/autoload.inc.php
@@ -23,35 +23,5 @@
 ?>
 <?php
 
-require_once dirname(__FILE__).'/autoload.inc.php';
-
-class ProjectConfiguration extends sfProjectConfiguration
-{
-  public $yob;
-  public $charset = array(
-    'db' => 'UTF-8',
-    'ms' => 'WINDOWS-1252//TRANSLIT',
-    'ascii' => 'ASCII//TRANSLIT',
-  );
-  public $transliterate = array(
-    'ŠŒŽšœžŸ¥µÀÁÂÃÄÅÆÇÈÉÊËÌÍÎÏÐÑÒÓÔÕÖØÙÚÛÜÝßàáâãäåæçèéêëìíîïðñòóôõöøùúûüýÿ',
-    'SOZsozYYuAAAAAAACEEEEIIIIDNOOOOOOUUUUYsaaaaaaaceeeeiiiionoooooouuuuyy');
-
-  protected $routings = array();
- 
-  
-  public function setup()
-  {
-    // year of birth
-    $this->yob = array();
-    for ( $i = 0 ; $i < 80 ; $i++ )
-      $this->yob[date('Y')-$i] = date('Y') - $i;
-    
-    $this->enablePlugins('sfDoctrinePlugin');
-    $this->enablePlugins('sfFormExtraPlugin');
-    $this->enablePlugins('sfDoctrineGraphvizPlugin');
-    $this->enablePlugins('sfDoctrineGuardPlugin');
-    $this->enablePlugins('sfAdminThemejRollerPlugin');
-    $this->enablePlugins('cxFormExtraPlugin');
-  }
-}
+require_once dirname(__FILE__).'/../lib/vendor/symfony/lib/autoload/sfCoreAutoload.class.php';
+sfCoreAutoload::register();
