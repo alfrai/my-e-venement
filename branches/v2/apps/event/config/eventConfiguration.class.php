a3a3f521113df4c5e1f06d0800de4bff0c6c7879
beta <beta@e52b1c2b-7d5d-4ba9-ac39-b163d13cfa43>
2011-03-21 12:54:49 +0000

    (event) adding a forgotten partial (event) modifying the eventConfiguration object for handling login/logout

--- branches/v2/apps/event/config/eventConfiguration.class.php
+++ branches/v2/apps/event/config/eventConfiguration.class.php
@@ -5,5 +5,24 @@ class eventConfiguration extends sfApplicationConfiguration
   public function configure()
   {
     sfConfig::set('sf_app_template_dir', sfConfig::get('sf_apps_dir') . '/templates');
+    
+    $this->dispatcher->connect('user.change_authentication', array($this, 'logAuthentication'));
+  }
+  
+  public function logAuthentication(sfEvent $event)
+  {
+    $params   = $event->getParameters();
+    $user     = sfContext::getInstance()->getUser();
+    $request  = sfContext::getInstance()->getRequest();
+    
+    $auth = new Authentication();
+    $auth->sf_guard_user_id = $user->getId();
+    $auth->description      = $user;
+    $auth->ip_address       = $request->getHttpHeader('addr','remote');
+    $auth->user_agent       = $request->getHttpHeader('User-Agent');
+    $auth->referer          = $request->getReferer();
+    $auth->success          = $params['authenticated'];
+    
+    $auth->save();
   }
 }
