a901bffde2bb1443a3866c10dfeb118633ab1b2c
beta <beta@e52b1c2b-7d5d-4ba9-ac39-b163d13cfa43>
2011-03-24 13:53:39 +0000

    (event) adding templates for manifestations (event) correcting geolocalization dimensions for manifestations

--- branches/v2/apps/event/modules/manifestation/templates/_gmap_one_only.php
+++ branches/v2/apps/event/modules/manifestation/templates/_gmap_one_only.php
@@ -2,7 +2,7 @@
   $gMap = new GMap();
   if ( $gMap->getGMapClient()->getAPIKey() ):
     $gMap = Addressable::getGmapFromObject($form->getObject()->Location, $gMap);
-    include_partial('global/gmap',array('gMap' => $gMap, 'width' => isset($width) ? $width : '420px', 'height' => '300px'));
+    include_partial('global/gmap',array('gMap' => $gMap, 'width' => isset($width) ? $width : '230px', 'height' => '230px'));
 ?>
 <?php else: ?>
   <p><?php echo __("The geolocalization module is not enabled, you can't access this function.") ?></p>
