19f7948a01c4fdfb8b9b786cd5ba87e3e6ae3cfa
beta <beta@e52b1c2b-7d5d-4ba9-ac39-b163d13cfa43>
2011-03-24 13:37:40 +0000

    (event) correcting the geolocalization display

--- branches/v2/apps/event/modules/location/templates/_gmap.php
+++ branches/v2/apps/event/modules/location/templates/_gmap.php
@@ -1,1 +1,1 @@
-<?php include_partial('global/gmap_one_only',array('form' => $form)); ?>
+<?php include_partial('global/gmap_one_only',array('form' => $form, 'width' => $width, 'height' => $height)); ?>
