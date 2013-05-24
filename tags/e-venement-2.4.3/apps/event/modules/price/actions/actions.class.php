3fb33e139346892b839a108922e2eee5f91e3437
beta <beta@e52b1c2b-7d5d-4ba9-ac39-b163d13cfa43>
2013-05-24 12:47:00 +0000

    new sub version 2.4.3 correcting bugs

---
+++ tags/e-venement-2.4.3/apps/event/modules/price/actions/actions.class.php
@@ -0,0 +1,28 @@
+<?php
+
+require_once dirname(__FILE__).'/../lib/priceGeneratorConfiguration.class.php';
+require_once dirname(__FILE__).'/../lib/priceGeneratorHelper.class.php';
+
+/**
+ * price actions.
+ *
+ * @package    e-venement
+ * @subpackage price
+ * @author     Baptiste SIMON <baptiste.simon AT e-glop.net>
+ * @version    SVN: $Id: actions.class.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
+ */
+class priceActions extends autoPriceActions
+{
+  public function executeIndex(sfWebRequest $request)
+  {
+    parent::executeIndex($request);
+    if ( !$this->sort[0] )
+    {
+      $this->sort = array('name','');
+      $a = $this->pager->getQuery()->getRootAlias();
+      $this->pager->getQuery()
+        ->select('*')
+        ->orderby("$a.hide, $a.value DESC, $a.name");
+    }
+  }
+}
---
+++ tags/e-venement-2.4.3/apps/rp/modules/contact/templates/_show_events_list.php
@@ -0,0 +1,77 @@
+<?php use_helper('Number') ?>
+<?php $contact = $form->getObject(); ?>
+
+<table>
+  <tbody>
+
+<?php
+  $pros = array(NULL);
+  foreach ( $contact->Professionals as $pro )
+    $pros[] = $pro;
+?>
+<?php foreach ( $pros as $pro ): ?>
+<?php
+  // hack for optimization
+  $q = Doctrine_Query::create()
+    ->from('MetaEvent me')
+    ->leftJoin('me.Events e')
+    ->leftJoin('e.Manifestations m')
+    ->leftJoin('m.Tickets tck')
+    ->leftJoin('tck.Transaction t')
+    ->where('t.contact_id = ?',$contact->id)
+    ->andWhere('tck.printed = TRUE OR tck.integrated = TRUE')
+    ->andWhere('tck.cancelling IS NULL AND tck.duplicating IS NULL')
+    ->andWhere('tck.id NOT IN (SELECT tck2.cancelling FROM Ticket tck2 WHERE tck2.cancelling IS NOT NULL)')
+    ->orderBy('me.name, e.name, m.happens_at DESC, t.id');
+
+  if ( is_null($pro) )
+    $q->andWhere('t.professional_id IS NULL');
+  else
+    $q->andWhere('t.professional_id = ?',$pro->id);
+
+  $meta_events = $q->execute();
+?>
+  <?php if ( $meta_events->count() > 0 ): ?>
+    <tr class="pro" title="<?php echo $pro ?>">
+      <td class="name" colspan="4"><span><?php echo $pro ?></span></td>
+    </tr>
+  <?php endif ?>
+  <?php foreach ( $meta_events as $me ): ?>
+    <tr class="meta_event">
+      <td class="name" colspan="4"><span><?php echo $me ?></span></td>
+    </tr>
+    <?php foreach( $me->Events as $event ): ?>
+    <tr class="event">
+      <td class="name" colspan="4" title="<?php echo $event ?>"><span><?php echo cross_app_link_to($event,'event','event/show?id='.$event->id) ?></span></td>
+    </tr>
+    <?php foreach( $event->Manifestations as $manif ): ?>
+    <tr class="manif">
+      <td class="name"><span><?php echo cross_app_link_to(format_date($manif->happens_at),'event','manifestation/show?id='.$manif->id) ?></span></td>
+      <td class="tickets_nb"><span><?php echo __('%%nb%% ticket(s)',array('%%nb%%' => $manif->Tickets->count())) ?></span></td>
+      <td class="tickets_value"><span><?php $value = 0; foreach ( $manif->Tickets as $ticket ) $value += $ticket->value; echo format_currency($value,'€'); ?></span></td>
+      <td class="transactions"><?php $arr = array(); foreach ( $manif->Tickets as $ticket ) $arr[$ticket->transaction_id] = '#'.cross_app_link_to($ticket->transaction_id,'tck','ticket/sell?id='.$ticket->transaction_id); echo implode(', ',$arr) ?></span></td>
+    </tr>
+    <?php endforeach ?>
+    <?php endforeach ?>
+  <?php endforeach ?>
+<?php endforeach ?>
+
+    <?php if ( $form->getObject()->EventArchives->count() > 0 ): ?>
+    <?php
+      $archives = array();
+      foreach ( $form->getObject()->EventArchives as $event )
+        $archives[$event->happens_at.$event->name.$event->id] = $event;
+      ksort($archives);
+    ?>
+    <tr class="archive meta_event">
+      <td colspan="4" class="name"><span><?php echo __('Archives') ?></span></td>
+    </tr>
+    <?php foreach ( array_reverse($archives) as $event ): ?>
+    <tr class="archive">
+      <td class="happens_at"><?php echo format_date($event->happens_at,'MMM yyyy') ?></td>
+      <td class="name" colspan="3" title="<?php echo $event->name ?>"><?php echo $event->name ?></td>
+    </tr>
+    <?php endforeach ?>
+    <?php endif ?>
+  </tbody>
+</table>
