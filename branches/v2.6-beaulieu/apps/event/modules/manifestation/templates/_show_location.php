bdcd88f29e2c05e105b2c21a3217d39c140c1b5a
beta <beta@e52b1c2b-7d5d-4ba9-ac39-b163d13cfa43>
2013-09-06 18:27:51 +0000

    replacing the beaulieu 2.6 branch

--- branches/v2.6-beaulieu/apps/event/modules/manifestation/templates/_show_location.php
+++
@@ -1,4 +0,0 @@
-<div class="sf_admin_form_row sf_admin_form_field_location">
-  <label><?php echo __('Location') ?>:</label>
-  <?php echo link_to($manifestation->Location,'location/show?id='.$manifestation->Location->id) ?>
-</div>
