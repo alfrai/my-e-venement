dcd81cb7cb50c1614f258e9c2f267ce77701ab10
beta <beta@e52b1c2b-7d5d-4ba9-ac39-b163d13cfa43>
2013-09-23 21:10:08 +0000

    (global) commit v2.6-pre4

---
+++ tags/e-venement-2.6-pre4/apps/event/modules/manifestation/templates/_show_event.php
@@ -0,0 +1,4 @@
+<div class="sf_admin_form_row sf_admin_form_field_event">
+  <label><?php echo __('Event') ?>:</label>
+  <?php echo link_to($manifestation->Event,'event/show?id='.$manifestation->Event->id) ?>
+</div>
