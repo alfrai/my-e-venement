9b4570cd87fd94774d4edb9d0540c9958a28a1e4
beta <beta@e52b1c2b-7d5d-4ba9-ac39-b163d13cfa43>
2014-03-03 13:33:50 +0000

    (global) v2.6.6

---
+++ tags/e-venement-2.6.6/apps/event/modules/manifestation/templates/_show_spectators_list_table_header.php
@@ -0,0 +1,12 @@
+<thead>
+  <tr>
+    <td class="name"><?php echo __('Contact') ?></td>
+    <td class="pro-groups"></td>
+    <td class="name"><?php echo __('Organism') ?></td>
+    <td class="qty"><?php echo __('Tickets') ?></td>
+    <td class="qty"><?php echo __('Value') ?></td>
+    <td class="accounting"><?php echo __('Acc.') ?></td>
+    <td class="transaction"><?php echo __('Transaction') ?></td>
+    <td class="ticket-ids"><?php echo __('Ticket ids') ?></td>
+  </tr>
+</thead>
