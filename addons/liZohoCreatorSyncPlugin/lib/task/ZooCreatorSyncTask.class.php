ac2f1562308e984b788465d8725bea66689bb8ce
beta <beta@e52b1c2b-7d5d-4ba9-ac39-b163d13cfa43>
2014-03-13 09:27:06 +0000

    (plugins) finishing the basics

--- addons/liZohoCreatorSyncPlugin/lib/task/ZooCreatorSyncTask.class.php
+++ addons/liZohoCreatorSyncPlugin/lib/task/ZooCreatorSyncTask.class.php
@@ -124,7 +124,6 @@ EOF;
   /**
    * function zoo2e imports Zoo Creator service's data into e-venement
    *
-   * @param $con, the liCardDavConnection object
    * @param $option, the task options from the execute() function
    *
    **/
@@ -140,7 +139,15 @@ EOF;
       $cpt = 0;
       foreach ( $response->body->{$distant['form']} as $object )
       {
-        $local = new $model;
+        if ( !isset($object->{$distant['uid']['distant']}) )
+        {
+          $this->logSection('Error', 'The current object does not have a UID', NULL, 'ERROR');
+          continue;
+        }
+        
+        $local = Doctrine::getTable($model)->findOneByVcardUid($object->{$distant['uid']['distant']});
+        if ( !$local )
+          $local = new $model;
         
         foreach ( $distant['fields'] as $field => $dfield )
         if ( is_array($dfield) && isset($dfield['name']) && isset($dfield['dist']) )
@@ -174,19 +181,22 @@ EOF;
         // saving the new object locally
         try
         {
-            $local->save();
-            $this->logSection($model.' '.$local->vcard_uid, (string)$local);
-            $cpt++;
-            
-            if ( $cpt%10 == 0 )
-            {
-              $before = memory_get_usage();
-              $dummy = clone $this;
-              $after = memory_get_usage();
-              $size = $after - $before;
-              unset($before, $after, $dummy);
-              $this->logSection('Memory', round(memory_get_usage()/1024/1024).' Mo / '.$size);
-            }
+          $info = $local->isNew() ? 'created' : $local->isModified() ? 'modified' : 'unmodified';
+          
+          $local->save();
+          $this->logSection($model.' '.$info, $local->vcard_uid.': '.$local);
+          
+          $cpt++;
+          
+          if ( $cpt%10 == 0 )
+          {
+            $before = memory_get_usage();
+            $dummy = clone $this;
+            $after = memory_get_usage();
+            $size = $after - $before;
+            unset($before, $after, $dummy);
+            $this->logSection('Memory', round(memory_get_usage()/1024/1024).' Mo / '.$size);
+          }
         }
         catch ( Doctrine_Connection_Exception $e ) // ERROR
         {
@@ -286,172 +296,11 @@ EOF;
   /**
    * function e2zoo exports e-venement's data into the Zoo Creator service
    *
-   * @param $con, the liCardDavConnection object
    * @param $option, the task options from the execute() function
    *
    **/
-  protected function e2zoo(liCardDavConnection $con, array $options)
+  protected function e2zoo(array $option)
   {
-    // add data
-    if ( in_array($options['model'], array('both', 'contact')) )
-      $this->e2zoo_sync($con, $options, Doctrine::getTable('Contact'), 'contact');
-    if ( in_array($options['model'], array('both', 'organism')) )
-      $this->e2zoo_sync($con, $options, Doctrine::getTable('Organism'), 'organism');
-    
-    // delete data
-    if (!( isset($options['no-del']) && $options['no-del'] ))
-    {
-      $tables = array();
-      $toprint = array('Contact' => 'contact', 'Organism' => 'organism');
-      if ( in_array($options['model'], array('both', 'contact')) )
-        $tables[] = 'Contact';
-      if ( in_array($options['model'], array('both', 'organism')) )
-        $tables[] = 'Organism';
-      $this->e2zoo_del($con, $options, $tables, $toprint);
-    }
-  }
-  
-  // DELETE vCards already deleted on e-venement
-  protected function e2zoo_del(liCardDavConnection $con, array $options, $tables, $toprint)
-  {
-    $table_name = 'sync_'.time(); // prepare a temporary table
-    $ids = $con->getIdsList();    // gets the remaining ids
-    $i = 0;
-    if ( count($ids) > 0 )
-    {
-      $pdo = Doctrine_Manager::getInstance()->getCurrentConnection()->getDbh();
-      
-      // creates the temp table
-      $stmt = $pdo->prepare("CREATE TEMP TABLE $table_name (id TEXT PRIMARY KEY);");
-      $stmt->execute();
-      
-      // inserts the ids
-      foreach ( $ids as $id )
-        $pdo->prepare("INSERT INTO $table_name VALUES ('$id');")->execute();
-
-      // retrieving deleted contacts
-      $where = array();
-      foreach ( $tables as $table )
-        $where[] = "SELECT vcard_uid FROM $table WHERE vcard_uid IS NOT NULL";
-      
-      $q = "SELECT id FROM $table_name WHERE id NOT IN (".implode(") AND id NOT IN (",$where).")";
-      $stmt = $pdo->prepare($q);
-      $stmt->execute();
-      
-      // deleting foreign data
-      foreach ( $stmt->fetchAll() as $uid )
-      {
-        $nb = str_pad(++$i,5,'0',STR_PAD_LEFT);
-        $vcard = NULL;
-        $vcard = $con->getVCard($uid['id']);
-        $contact_str = mb_str_pad($vcard['fn'], 30);
-        $this->logSection('e2zoo', sprintf('%s %s %s has been deleted (uid %s)', $nb, implode(' or ', $toprint), $contact_str, $uid['id']), null, 'COMMAND');
-        
-        $vcard->delete();
-        $cpt['deleted']++;
-      }
-    }
-  }
-  protected function e2zoo_sync(liCardDavConnection $con, array $options, $table, $toprint)
-  {
-    $cpt = array(
-      'up2date' => 0,
-      'uploaded' => 0,
-      'added' => 0,
-      'deleted' => 0,
-    );
-    
-    $q = $table->createQuery('c')
-      ->limit(isset($options['nb']) ? intval($options['nb']) : 0)
-      ->orderBy('c.created_at, c.updated_at DESC')
-      ;
-    if (!( isset($options['force']) && $options['force'] ))
-      $q->andWhere('c.updated_at >= ?', date('Y-m-d H:i:s', strtotime($con->getLastUpdate())));
-    
-    $i = 0;
-    foreach ( $q->execute() as $object )
-    {
-      $nb = str_pad(++$i,5,'0',STR_PAD_LEFT);
-      $object_str = mb_str_pad($object, 30);
-      
-      sfConfig::set('app_zoocreator_sync_timezone_hack', true); // to be used by Contact::getVcard()
-      $vcard = array('e' => liCardDavVCard::create($con, $object->vcard_uid, (string)$vc = $object->vcard));
-      
-      // try to stop the process if the distant data is up2date or exists in a newer version
-      if ( $object->vcard_uid )
-      {
-        $vcard['zoo'] = liCardDavVCard::create($con, $object->vcard_uid);
-        
-        if ( isset($vcard['zoo']['rev']) && strtotime($vcard['zoo']['rev']) >= strtotime($vcard['e']['rev']) )
-        {
-          $cpt['up2date']++;
-          $this->weirds[] = $object;
-          $this->logSection('e2zoo', sprintf('%s %s %s has been kept (uid %s)', $nb, $toprint, $object_str, $object->vcard_uid), null, 'COMMAND');
-          
-          // debug
-          if ( $options['debug'] )
-          {
-            echo sprintf("distant: %s/%s >= local: %s/%s\n\n", $vcard['zoo']['rev'], strtotime($vcard['zoo']['rev']), $vcard['e']['rev'], strtotime($vcard['e']['rev']));
-            echo $vcard['zoo']."\n";
-          }
-          
-          continue;
-        }
-      }
-      
-      // local data needs to be sent to the CardDAV repository -> create or update
-      if ( $options['env'] != 'dev' ) // PROD ENV - for real
-      {
-        // try to delete to fake updating
-        $deleted = true;
-        try
-        {
-          if ( isset($vcard['zoo']['uid']) )
-            $vcard['zoo']->delete();
-          else
-          {
-            $vcard['e']->turnNew();
-            $deleted = false;
-          }
-        }
-        catch ( liCardDavResponse404Exception $e )
-        { $delete = false; }
-        
-        // adding the object
-        $response = $vcard['e']->save();
-        $object->vcard_uid = $response->getUid();
-        $object->save();
-        
-        $cpt[$deleted ? 'uploaded' : 'added']++;
-        $this->logSection('e2zoo', sprintf('%s %s %s has been sent (uid %s)', $nb, $toprint, $object_str, $object->vcard_uid), null, 'COMMAND');
-      }
-      else // DEVELOPMENT ENV - tests only
-      {
-        // check if the object exists, so would be deleted
-        $delete = true;
-        try { $vcard['zoo']->update(); }
-        catch ( liCardDavResponse404Exception $e )
-        { $delete = false; }
-        
-        $cpt[$deleted ? 'uploaded' : 'added']++;
-        $this->logSection('e2zoo', sprintf('%s %s %s has not been sent (uid %s)', $nb, $toprint, $object_str, $object->vcard_uid), null, 'ERROR');
-      }
-      
-      // debug code
-      if ( $options['debug'] )
-      {
-        if ( isset($vcard['zoo']) )
-          echo sprintf("distant: %s/%s < local: %s/%s\n\n", $vcard['zoo']['rev'], strtotime($vcard['zoo']['rev']), $vcard['e']['rev'], strtotime($vcard['e']['rev']));
-        echo $vcard['e']."\n";
-      }
-      
-    }
-    
-    $this->logSection('e2zoo', sprintf('%d %s(s) added into the zoo repository', $cpt['added'], $toprint));
-    $this->logSection('e2zoo', sprintf('%d %s(s) that have been updated in the Zoo Creator repository', $cpt['uploaded'], $toprint));
-    $this->logSection('e2zoo', sprintf('%d %s(s) that did not need any synchronization', $cpt['up2date'], $toprint));
-    //$this->logSection('e2zoo', sprintf('%d %s(s) have been deleted from the Zoo Creator repository', $cpt['deleted'], $toprint));
-    
-    return $this;
+    $this->logSection('Not Implemented', 'This feature is not yet available.', NULL, 'ERROR');
   }
 }
