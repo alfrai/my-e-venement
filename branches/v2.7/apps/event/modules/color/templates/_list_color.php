437bfd1a26a64dd87b04f300293529644598e069
beta <beta@e52b1c2b-7d5d-4ba9-ac39-b163d13cfa43>
2014-07-23 09:01:53 +0000

    correcting last commit

--- branches/v2.7/apps/event/modules/color/templates/_list_color.php
+++
@@ -1,3 +0,0 @@
-<div style="padding: 5px; text-align: center; color: gray; background-color: <?php echo $color ?>">
-  <?php echo $color ?>
-</div>
--- branches/v2.7/apps/event/modules/manifestation/templates/_list_description.php
+++
@@ -1,4 +0,0 @@
-<div>
-  <?php echo mb_substr($manifestation->description,0,85) ?>
-  <?php echo mb_strlen($manifestation->description) > 80 ? ' ...' : '' ?>
-</div>
