eb27fd0fc6a35089cbf59273ab2202cfee7f957c
beta <beta@e52b1c2b-7d5d-4ba9-ac39-b163d13cfa43>
2014-07-23 08:59:39 +0000

    removing a branch

--- branches/e-venement-v2.7-RC1/apps/pub/modules/event/templates/_dates.php
+++
@@ -1,8 +0,0 @@
-<?php if ( $event->Manifestations->count() > 0 ): ?>
-<ul>
-<?php foreach ( $event->Manifestations as $manif ): ?>
-  <li class="month-<?php echo format_date(strtotime($manif->happens_at), 'yyyyMM') ?>" data-time="<?php echo strtotime($manif->happens_at) ?>"><?php echo link_to($manif->getFormattedDate(),'manifestation/edit?id='.$manif->id) ?></li>
-<?php endforeach ?>
-</ul>
-<?php endif ?>
-
