b5b3d6e5a3942e0abed6591619f9709ab9963ecd
beta <beta@e52b1c2b-7d5d-4ba9-ac39-b163d13cfa43>
2014-12-19 12:06:39 +0000

    (doctrine) correcting a bug that occurs when using multiple DB connections & behaviors "temporary tables"

--- symfony/trunk/lib/plugins/sfDoctrinePlugin/lib/vendor/doctrine/Doctrine/Record/Generator.php
+++ symfony/trunk/lib/plugins/sfDoctrinePlugin/lib/vendor/doctrine/Doctrine/Record/Generator.php
@@ -163,7 +163,7 @@ abstract class Doctrine_Record_Generator extends Doctrine_Record_Abstract
         // check that class doesn't exist (otherwise we cannot create it)
         if ($this->_options['generateFiles'] === false && class_exists($this->_options['className'])) {
             $this->_table = Doctrine_Core::getTable($this->_options['className']);
-            return false;
+            //return false;
         }
 
         $this->buildTable();
@@ -461,7 +461,7 @@ abstract class Doctrine_Record_Generator extends Doctrine_Record_Abstract
             } else {
                 throw new Doctrine_Record_Exception('If you wish to generate files then you must specify the path to generate the files in.');
             }
-        } else {
+        } elseif ( !class_exists($definition['className']) ) {
             $def = $builder->buildDefinition($definition);
 
             eval($def);
